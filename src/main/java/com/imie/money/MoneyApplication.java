44bf404ab57f913112898a3162b549b04eda4b05
Remi35 <remi.salembier@gmail.com>
2018-08-22 16:57:42 +0200

    Spring boot application !

---
+++ src/main/java/com/imie/money/MoneyApplication.java
@@ -0,0 +1,17 @@
+package com.imie.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan("com.imie.money")
+public class MoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyApplication.class, args);
+    }
+}
+

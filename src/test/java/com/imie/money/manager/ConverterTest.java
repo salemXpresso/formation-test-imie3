a6df984095a6cfb50f27f64b0842976ce9a92010
Remi35 <remi.salembier@gmail.com>
2018-08-22 15:32:01 +0200

    Converter unit tests update with currency management

--- src/test/java/com/imie/money/manager/ConverterTest.java
+++ src/test/java/com/imie/money/manager/ConverterTest.java
@@ -2,17 +2,28 @@ package com.imie.money.manager;
 
 import com.imie.money.model.Money;
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Currency;
 
 public class ConverterTest {
 
+    private CurrencyRate currencyRate;
+    private Converter converter;
+
+    @Before
+    public void setUp() {
+        currencyRate = mock(CurrencyRate.class);
+        converter = new Converter(currencyRate);
+    }
+
     @Test
     public void test_add_nominal_returnExpectedResult() {
 
         // Init context & mocks
-        Converter converter = new Converter();
         Money m1 = new Money(Currency.getInstance("EUR"), 2.15f);
         Money m2 = new Money(Currency.getInstance("EUR"), 2.85f);
 
@@ -27,7 +38,6 @@ public class ConverterTest {
     public void test_add_sameCurrencyNegativeValue_returnIllegalEx() {
 
         // Init context & mocks
-        Converter converter = new Converter();
         Money m1 = new Money(Currency.getInstance("EUR"), -2.15f);
         Money m2 = new Money(Currency.getInstance("EUR"), 2.85f);
 
@@ -39,22 +49,26 @@ public class ConverterTest {
     public void test_add_NullMoney_ReturnIllegalEx() {
 
         // Init context & mocks
-        Converter converter = new Converter();
         Money m2 = new Money(Currency.getInstance("EUR"), 2.85f);
 
         // Execution
         converter.add(null, m2);
     }
 
-    @Test(expected = UnsupportedOperationException.class)
-    public void test_add_differentCurrency_returnUnsupportedEx() {
+    @Test
+    public void test_add_differentCurrency_returnCurrency1AndRatedResult() {
 
         // Init context & mocks
-        Converter converter = new Converter();
-        Money m1 = new Money(Currency.getInstance("USD"), 2.15f);
-        Money m2 = new Money(Currency.getInstance("EUR"), 2.85f);
+        Currency c1 = Currency.getInstance("USD");
+        Currency c2 = Currency.getInstance("EUR");
+        Money m1 = new Money(c1, 2f);
+        Money m2 = new Money(c2, 2f);
+        when(currencyRate.getCurrencyRate(c1, c2)).thenReturn(1.2f);
 
         // Execution
-        converter.add(m1, m2);
+        Money m = converter.add(m1, m2);
+
+        // Assertions
+        assertThat(m).isEqualToComparingFieldByField(new Money(c1, 4.4f));
     }
 }

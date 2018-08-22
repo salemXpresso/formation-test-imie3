ca2c95520b0790b9a0576873e603b33743713bbe
Remi35 <remi.salembier@gmail.com>
2018-08-22 16:35:55 +0200

    Currency rates are now loaded from a ws call on startup

--- src/main/java/com/imie/money/manager/CurrencyRate.java
+++ src/main/java/com/imie/money/manager/CurrencyRate.java
@@ -1,5 +1,8 @@
 package com.imie.money.manager;
 
+import com.imie.money.connector.CurrencyRateWsClient;
+
+import java.io.IOException;
 import java.util.Currency;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,16 +10,11 @@ import java.util.Map;
 public class CurrencyRate {
 
     protected Map<Currency, Float> ratesWithRef = new HashMap<>();
-    private Currency USD = Currency.getInstance("USD");
-    private Currency EUR = Currency.getInstance("EUR");
-    private Currency GBP = Currency.getInstance("GBP");
 
+    public CurrencyRate(CurrencyRateWsClient currencyRateWsClient, String apiKey) throws IOException {
 
-    public CurrencyRate() {
-        // Currency reference = USD
-        ratesWithRef.put(USD, 1f);
-        ratesWithRef.put(EUR, 1.16f);
-        ratesWithRef.put(GBP, 1.29f);
+        Currency reference = Currency.getInstance("USD");
+        ratesWithRef = currencyRateWsClient.downloadLatestCurrencyRates(apiKey, reference);
     }
 
     public float getCurrencyRate(Currency c1, Currency c2) {

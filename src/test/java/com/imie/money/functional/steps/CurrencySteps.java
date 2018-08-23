12272e7098db72821136ce61c65e34f76dea7e5b
Remi35 <remi.salembier@gmail.com>
2018-08-23 16:35:16 +0200

    Addition of all Cucumber steps

--- src/test/java/com/imie/money/functional/steps/CurrencySteps.java
+++ src/test/java/com/imie/money/functional/steps/CurrencySteps.java
@@ -4,7 +4,13 @@ import com.imie.money.functional.driverfactory.DriverFactory;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.assertj.core.data.Offset;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -23,14 +29,80 @@ public class CurrencySteps {
         System.out.println("Should be running");
     }
 
-    @When("the user opens the page 'je voyage'")
+    @When("^the user opens the page 'je voyage'$")
     public void openJeVoyage() {
 
         webDriver.get("https://www.visa.fr/je-voyage/");
     }
 
-    @Then("the url on the browser contains 'je-voyage'")
+    @Then("^the url on the browser contains 'je-voyage'$")
     public void checkUrl() {
         assertThat(webDriver.getCurrentUrl()).contains("je-voyage");
+    }
+
+    @When("^the user clicks on 'mes outils pratiques'$")
+    public void clickMesOutils() {
+
+        webDriver.findElement(By.id("MainContent_LeftNav1_outerRepeater_HyperLink2_4")).click();
+    }
+
+    @When("^the user clicks on 'convertisseur de devises'$")
+    public void clickConvertisseur() {
+
+        webDriver.findElement(By.id("MainContent_LeftNav1_outerRepeater_innerRepeater_4_HyperLink2_2")).click();
+    }
+
+    @Then("^the currency form is displayed$")
+    public void checkCurrencyFormDisplayed() {
+
+        WebElement formElement = webDriver.findElement(By.id("MainContent_MainContent_ctl00_lblCardCurrency"));
+        assertThat(formElement).isNotNull();
+    }
+
+    @When("^the user selects <(.*)> for his card$")
+    public void selectCard(String currency) {
+        Select select = new Select(webDriver.findElement(By.id("MainContent_MainContent_ctl00_ddlCardCurrency")));
+        select.selectByValue(currency);
+    }
+
+    @When("^the user selects <(.*)> for his transaction$")
+    public void selectTransaction(String currency) {
+        Select select = new Select(webDriver.findElement(By.id("MainContent_MainContent_ctl00_ddlTransactionCurrency")));
+        select.selectByValue(currency);
+    }
+
+    @When("^the user submits the form$")
+    public void submitForm() {
+        webDriver.findElement(By.id("MainContent_MainContent_ctl00_btnSubmit")).click();
+    }
+
+    @Then("^the result page is displayed$")
+    public void checkResultDisplayed() {
+        List<WebElement> webElementList = webDriver.findElements(By.tagName("h1"));
+
+        boolean resultatBoxIsDisplayed = false;
+        for(WebElement anElement : webElementList) {
+            String text = anElement.getText();
+            if("Résultats".equals(text)) {
+                resultatBoxIsDisplayed = true;
+                break;
+            }
+        }
+        assertThat(resultatBoxIsDisplayed).isTrue();
+    }
+
+    @Then("^the conversion result is <(.*)>$")
+    public void checkResult(float expectedResult) {
+
+        List<WebElement> paragraphs = webDriver.findElement(By.className("col-md-8")).findElement(By.className("box-grey")).findElements(By.className("col-lg-12"));
+        List<WebElement> boldTextList = paragraphs.get(1).findElement(By.tagName("p")).findElements(By.tagName("strong"));
+        String result = boldTextList.get(1).getText();
+        float parsedResult = Float.parseFloat(result.replace(',', '.'));
+        assertThat(parsedResult).isCloseTo(expectedResult, Offset.offset(0.1f));
+    }
+
+    @Then("^The user closes his Web browser$")
+    public void closeBrowser() {
+        webDriver.close();
     }
 }
--- src/test/resources/com/imie/money/functional/currencyForm.feature
+++ src/test/resources/com/imie/money/functional/currencyForm.feature
@@ -8,16 +8,18 @@ Feature: Currency Calculator form
     When the user opens the page 'je voyage'
     Then the url on the browser contains 'je-voyage'
 
-    #When the user clicks on 'mes outils pratiques'
-    #And the user clicks on 'convertisseur de devises'
-    #Then the currency form is displayed
+    When the user clicks on 'mes outils pratiques'
+    And the user clicks on 'convertisseur de devises'
+    Then the currency form is displayed
 
-  #When the user selects 'EUR' for his card
-  #  And the user selects 'GBP' for his transaction
-  # And the user submits the form
-  # Then the result page is displayed
-# And the conversion result is 'xyz'
+    When the user selects <EUR> for his card
+    And the user selects <GBP> for his transaction
+    And the user submits the form
+    Then the result page is displayed
+    And the conversion result is <111.62>
 
+    # Post requisites
+    And The user closes his Web browser
 
 
 
